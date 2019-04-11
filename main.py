289d4b57e428aab5ccf0bbf789e5069f47dcf6d3
ThaddeusXx <ThaddeusXx@foxmail.com>
2019-04-11 15:44:41 +0800

    mod input_size/batch & LR for two-classifiers

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=100, type=int,
-                    metavar='N', help='print frequency (default: 100)')
+                    metavar='N', help='display frequency (default: 100)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
